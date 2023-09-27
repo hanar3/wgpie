e176e1aed9328f31093085989738ad418dedced4
vh.morenodasilva@gmail.com <vh.morenodasilva@gmail.com>
2023-09-27 15:08:11 -0300

    ortographic camera now works and we can use screen coordinates!

--- src/ui_scene.rs
+++ src/ui_scene.rs
@@ -1,4 +1,9 @@
+extern crate lyon;
 use cgmath::{Rotation3, SquareMatrix};
+use lyon::geom::Box2D;
+use lyon::math::point;
+use lyon::path::Path;
+use lyon::tessellation::*;
 use wgpu::util::DeviceExt;
 use winit::event::{ElementState, KeyboardInput, VirtualKeyCode, WindowEvent};
 
@@ -22,47 +27,139 @@ impl Vertex {
     }
 }
 
-const VERTICES: &[Vertex] = &[
-    Vertex {
-        position: [-0.5, -0.5, 0.0],
-        color: [0.5, 0.0, 0.0],
-    }, // A
-    Vertex {
-        position: [0.5, -0.5, 0.0],
-        color: [0.5, 0.0, 0.0],
-    }, // B
-    Vertex {
-        position: [0.5, 0.5, 0.0],
-        color: [0.5, 0.0, 0.0],
-    }, // C
-    Vertex {
-        position: [-0.5, 0.5, 0.0],
-        color: [0.5, 0.0, 0.0],
-    } // C
+fn screen_to_cartesian(
+    screen_x: f32,
+    screen_y: f32,
+    x_min: f32,
+    x_max: f32,
+    y_min: f32,
+    y_max: f32,
+    screen_width: f32,
+    screen_height: f32,
+) -> (f32, f32) {
+    let cartesian_x = (screen_x / screen_width) * (x_max - x_min) + x_min;
+    let cartesian_y = ((screen_height - screen_y) / screen_height) * (y_max - y_min) + y_min;
 
-];
-const INDICES: &[u16] = &[
-    0, 1, 2,
-    0, 2, 3
-];
+    (cartesian_x, cartesian_y)
+}
+
+pub const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::new(
+    1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.5, 0.5, 0.0, 0.0, 0.0, 1.0,
+);
+
+pub struct Camera {
+    eye: cgmath::Point3<f32>,
+    target: cgmath::Point3<f32>,
+    up: cgmath::Vector3<f32>,
+    aspect: f32,
+    fovy: f32,
+    znear: f32,
+    zfar: f32,
+}
+
+impl Camera {
+    fn build_view_projection_matrix(&self) -> cgmath::Matrix4<f32> {
+        let view = cgmath::Matrix4::look_at_rh(self.eye, self.target, self.up);
+        let proj = cgmath::ortho(0.0, 0.0, 0.0, 0.0, 0.1, 100.0);
+        return OPENGL_TO_WGPU_MATRIX * proj * view;
+    }
+}
+
+#[repr(C)]
+#[derive(Debug, Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
+pub struct CameraUniform {
+    view_proj: [[f32; 4]; 4],
+}
+
+impl CameraUniform {
+    fn new() -> Self {
+        Self {
+            view_proj: cgmath::Matrix4::identity().into(),
+        }
+    }
+
+    fn update_view_proj(&mut self, camera: &Camera) {
+        self.view_proj = camera.build_view_projection_matrix().into();
+    }
+
+    fn from_camera(camera: &Camera) -> Self {
+        Self {
+            view_proj: camera.build_view_projection_matrix().into(),
+        }
+    }
+}
 
 pub struct UIScene {
+    pub vertices: Vec<Vertex>,
+    pub indices: Vec<u16>,
     pub render_pipeline: wgpu::RenderPipeline,
     pub vertex_buffer: wgpu::Buffer,
     pub index_buffer: wgpu::Buffer,
+    pub bind_group: wgpu::BindGroup,
 }
 
 impl UIScene {
     pub async fn new(device: &wgpu::Device, config: &wgpu::SurfaceConfiguration) -> Self {
+        let m_box = Box2D::new(point(0.0, 0.0), point(-50.0, -50.0));
+
+        let aspect = (config.width / config.height) as f32;
+        let half_height = config.height as f32 / 2.0; // also called ortho size
+        let half_width = half_height * aspect;
+
+        let mut geometry: VertexBuffers<Vertex, u16> = VertexBuffers::new();
+        let mut tessellator = FillTessellator::new();
+
+        {
+            // Compute the tessellation.
+            tessellator
+                .tessellate_rectangle(
+                    &m_box,
+                    &FillOptions::default(),
+                    &mut BuffersBuilder::new(&mut geometry, |vertex: FillVertex| Vertex {
+                        position: [vertex.position().x, vertex.position().y, -0.0],
+                        color: [0.5, 0.0, 0.0],
+                    }),
+                )
+                .unwrap();
+        }
+        let vertices = geometry.vertices.to_vec();
+        let indices = geometry.indices.to_vec();
+
         let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
             label: Some("shader"),
             source: wgpu::ShaderSource::Wgsl(include_str!("ui_shader.wgsl").into()),
         });
 
+        let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            label: Some("ui_bind_group"),
+            entries: &[
+                wgpu::BindGroupLayoutEntry {
+                    binding: 0, // surface size buffer
+                    visibility: wgpu::ShaderStages::FRAGMENT,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding: 1, // orthographic matrix
+                    visibility: wgpu::ShaderStages::VERTEX,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
+                },
+            ],
+        });
+
         let render_pipeline_layout =
             device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
                 label: Some("UI Render pipeline layout"),
-                bind_group_layouts: &[],
+                bind_group_layouts: &[&bind_group_layout],
                 push_constant_ranges: &[],
             });
 
@@ -88,7 +185,7 @@ impl UIScene {
             primitive: wgpu::PrimitiveState {
                 topology: wgpu::PrimitiveTopology::TriangleList, // 1.
                 strip_index_format: None,
-                front_face: wgpu::FrontFace::Ccw, // 2.
+                front_face: wgpu::FrontFace::Cw, // 2.
                 cull_mode: Some(wgpu::Face::Back),
                 // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
                 polygon_mode: wgpu::PolygonMode::Fill,
@@ -106,22 +203,62 @@ impl UIScene {
             multiview: None, // 5.
         });
 
+        // Creating uniforms
+        let screen_size_uniform = &[config.width as f32, config.height as f32];
+        let screen_size_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("screensize_uniform"),
+            contents: bytemuck::cast_slice(screen_size_uniform),
+            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+        });
+
+        let projection = cgmath::ortho(-half_width, half_width,-half_height, half_height, -1.0, 1.0);
+        let rotation = cgmath::Quaternion::from_axis_angle(cgmath::Vector3::unit_z(), cgmath::Deg(0.0));
+        let transform = cgmath::Matrix4::from_translation(cgmath::Vector3::new(0.0, 0.0, 0.0)) * cgmath::Matrix4::from(rotation);
+        let view_matrix = cgmath::Matrix4::invert(&transform).unwrap();
+        let view_projection_matrix: [[f32; 4]; 4] = (projection * view_matrix).into();
+
+        let ortho_proj_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("uOrthoProj"),
+            contents: bytemuck::cast_slice(&view_projection_matrix),
+            usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+        });
+
+        // Vertex buffer
         let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
             label: Some("UI Vertex Buffer"),
-            contents: bytemuck::cast_slice(VERTICES),
+            contents: bytemuck::cast_slice(geometry.vertices.as_slice()),
             usage: wgpu::BufferUsages::VERTEX | wgpu::BufferUsages::COPY_DST,
         });
 
+        // Index buffer
         let index_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
             label: Some("UI Index buffer"),
-            contents: bytemuck::cast_slice(INDICES),
+            contents: bytemuck::cast_slice(geometry.indices.as_slice()),
             usage: wgpu::BufferUsages::INDEX,
         });
 
+        let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: Some("ui_bind_group"),
+            layout: &bind_group_layout,
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: screen_size_buffer.as_entire_binding(),
+                },
+                wgpu::BindGroupEntry {
+                    binding: 1,
+                    resource: ortho_proj_buffer.as_entire_binding(),
+                },
+            ],
+        });
+
         Self {
+            vertices,
+            indices,
             render_pipeline,
             vertex_buffer,
             index_buffer,
+            bind_group,
         }
     }
 
@@ -129,6 +266,56 @@ impl UIScene {
 
     pub fn input(&mut self, event: &WindowEvent) -> bool {
         false
+    }
+
+    fn rect(x: f32, y: f32, w: f32, h: f32) -> (Vec<Vertex>, Vec<u16>) {
+        let screen_start_pos = cgmath::Vector2::new(x, y);
+        let screen_end_pos = screen_start_pos + cgmath::Vector2::new(w, h);
+
+        // Start point
+        let xy1 = cgmath::Vector2::from(screen_to_cartesian(
+            screen_start_pos.x,
+            screen_start_pos.y,
+            -1.0,
+            1.0,
+            -1.0,
+            1.0,
+            800.0,
+            600.0,
+        ));
+
+        // End point (in cartesian)
+        let xy2 = cgmath::Vector2::from(screen_to_cartesian(
+            screen_end_pos.x,
+            screen_end_pos.y,
+            -1.0,
+            1.0,
+            -1.0,
+            1.0,
+            800.0,
+            600.0,
+        ));
+
+        let mut vertices = vec![];
+        vertices.push(Vertex {
+            position: [xy1.x, xy1.y, 0.0],
+            color: [0.5, 0.0, 0.0],
+        }); // -0.5, -0.5
+        vertices.push(Vertex {
+            position: [xy2.x, xy1.y, 0.0],
+            color: [0.5, 0.0, 0.0],
+        }); // 0.5, -0.5
+        vertices.push(Vertex {
+            position: [xy2.x, xy2.y, 0.0],
+            color: [0.5, 0.0, 0.0],
+        }); // 0.5, 0.5
+        vertices.push(Vertex {
+            position: [xy1.x, xy2.y, 0.0],
+            color: [0.5, 0.0, 0.0],
+        }); // -0.5, 0.5
+
+        let indices = vec![2, 1, 0, 2, 0, 3];
+        return (vertices, indices);
     }
 
     pub fn update(&mut self, queue: &wgpu::Queue) {}
@@ -148,8 +335,9 @@ impl UIScene {
         });
 
         render_pass.set_pipeline(&self.render_pipeline);
+        render_pass.set_bind_group(0, &self.bind_group, &[]);
         render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
         render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
-        render_pass.draw_indexed(0..INDICES.len() as u32, 0, 0..1);
+        render_pass.draw_indexed(0..self.indices.len() as u32, 0, 0..1);
     }
 }
--- src/ui_shader.wgsl
+++ src/ui_shader.wgsl
@@ -7,8 +7,8 @@ struct VertexInput {
 
 struct VertexOutput {
     @builtin(position) clip_position: vec4<f32>,
-    @location(0) fragCoords: vec2<f32>,
 };
+
 
 
 fn sd_round_box(point: vec2<f32>, size: vec2<f32>, r: vec4<f32>)-> f32 {
@@ -32,33 +32,24 @@ fn sd_round_box(point: vec2<f32>, size: vec2<f32>, r: vec4<f32>)-> f32 {
     return min(max(q.x,q.y),0.0) + length(max(q, vec2<f32>(0.0, 0.0))) - radius.x;
 }
 
+@group(0) @binding(1)
+var<uniform> proj: mat4x4<f32>;
+
+
 @vertex
 fn vs_main(
     model: VertexInput,
 ) -> VertexOutput {
     var out: VertexOutput;
-    out.clip_position = vec4<f32>(model.position, 1.0);
-    out.fragCoords = vec2<f32>(model.position.x, model.position.y);
+    out.clip_position = proj * vec4<f32>(model.position, 1.0);
     return out;
 }
 
 
+@group(0) @binding(0)
+var<uniform> screen_size: vec2<f32>;
 
 @fragment
 fn fs_main(in: VertexOutput) -> @location(0) vec4<f32>{
-    var screen_size = vec2<f32>(800.0, 600.0); // Needs to be a uniform
-    var rect_size = vec2<f32>(400.0, 300.0); // Needs to be a uniform
-
-    // This gives us values from -1.0 to 1.0 that are relative to the rectangle 
-    // such that the top left of the rect is -1.0, -1.0 - just makes it easier
-    // to run mathematical formulas on this
-    var point = 1.0 * in.fragCoords / (rect_size / screen_size);  
-    var box_size = vec2<f32>(0.95, 0.95);
-
-    var d = sd_round_box(point, box_size, vec4<f32>(0.5, 0.02, 0.02, 0.02));
-    if (d > 0.0) {
-        return vec4<f32>(1.0, 0.0, 0.0, 0.0);
-    }
-
-    return vec4<f32>(0.0, 1.0, 0.0, 0.0);
+    return vec4<f32>(1.0, 0.0, 1.0, 1.0);
 }
